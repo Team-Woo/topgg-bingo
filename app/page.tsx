c7cb23eb67939d3604b72fa05a5244502f62b5aa
Woo <woo33044@gmail.com>
2023-04-28 15:59:12 -0400

    Init?

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,44 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+'use client';
+import Image from "next/image"
+import { Dispatch, SetStateAction, useState } from "react"
+function bingoSlot(value: string, key: number) {
+  const [selected, setSelected] = useState(false);
+  return(
+    <div className="w-36 h-36 text-center border-2 border-[#ff3366] bg-[#070510] text-[#ff3366] text-xl font-semibold flex items-center justify-center" onClick={() => setSelected(!selected)}>
+      {selected ? <Image src="/TopggLogo.png" alt="Topgg Logo" width={128} height={20} /> : value}
+    </ div>
+  ) 
+}
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex flex-col items-center gap-5">
+        <div className="flex flex-row pt-5 gap-3">
+          <h1 className="text-[40px] font-semibold">Top.gg</h1>
+          <Image src="/TopggLogo.png" alt="Topgg Logo" width={150} height={20} />
+        </div>
+        <div className="text-[30px] font-semibold">Woo Bingo</div>
+
+        <div id="boardContianer">
+          <div id="board" className="border-[#ff3366] border-2 grid grid-cols-5 gap-0">
+            {
+              cards.map((Value, index) => {
+                return(
+                  bingoSlot(Value, index)
+                )
+              })
+            }
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
+
+const cards = [
+  "Aurel answers", "Random sticker chain", "We're back", "Was not logged in/Wrong account", "Luca fails to give bot dev role",
+  "526", "525", "The bot went offline during review", "504", "Why dont you just recruit more reviewers?",
+  "Slowmode is enabled", "Scam attempt", "Voting is down", "Innapropiate content in #general", "I submit my bot 34 years ago, why not review??",
+  "There's an engineer looking into it", "502", "Pls give mod", "Unmodified bot clone", "503",
+  "-no/lang", "Can I see the queue?", "Bot-specific question", "Team member appears", "500",
+]
